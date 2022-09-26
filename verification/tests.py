432539ac25c14124cb3da5e6ec1c2790fae3a494
Alexander Blaschuk <blaschuk95@gmail.com>
2022-09-26 15:33:24 +0300

    next-api

--- verification/tests.py
+++ verification/tests.py
@@ -11,25 +11,25 @@ Each test is a dict with
 TESTS = {
     "Basics": [
         {
-            "input": "01.01.2000 00:00",
+            "input": ["01.01.2000 00:00"],
             "answer": "1 January 2000 year 0 hours 0 minutes"
         },
         {
-            "input": "09.05.1945 06:07",
+            "input": ["09.05.1945 06:07"],
             "answer": "9 May 1945 year 6 hours 7 minutes"
         }
     ],
     "Extra": [
         {
-            "input": "20.11.1990 03:55",
+            "input": ["20.11.1990 03:55"],
             "answer": "20 November 1990 year 3 hours 55 minutes"
         },
         {
-            "input": "09.07.1995 16:01",
+            "input": ["09.07.1995 16:01"],
             "answer": "9 July 1995 year 16 hours 1 minute"
         },
 	{
-            "input": "11.04.1812 01:01",
+            "input": ["11.04.1812 01:01"],
             "answer": "11 April 1812 year 1 hour 1 minute"
         }
     ]
